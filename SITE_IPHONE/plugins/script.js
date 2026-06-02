f8b26c3d75dffc99a5c9a838e1e20e010a1ba874
Sabrina Melo do Nascimento <252.14.062@EDU.gruposete.local>
2026-06-01 21:23:31 -0300

    site iphone

---
+++ SITE_IPHONE/plugins/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $("#banners ul").bxSlider({
+        auto: true,
+        speed: 1000, // 1000milisegundos
+         //mode: 'fade' //aparece
+        // pager: false //pontinhos
+        // mode: 'vertical' //de cima para baixo
+    });
+
+    $('#fotos #galeria').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+
+    });
+});
+
